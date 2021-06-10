ed98cac86dff5e63db7ddb21fad9dce77173a121
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-06-10 23:17:30 +0500

    Update about.js

---
+++ demo1/pages/about.js
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium voluptatibus numquam nemo et nulla, exercitationem soluta maxime similique placeat molestiae sunt debitis? Cum libero autem reiciendis, illo laboriosam cupiditate sapiente blanditiis deserunt iusto dolore a minus ab hic sunt corporis repellendus, consequuntur excepturi praesentium magnam impedit. Quam eveniet sit incidunt, enim voluptatem quas molestiae nobis qui et in temporibus necessitatibus neque iste facere ea cumque rerum ipsum sequi. Doloribus et soluta repellat maxime cum, reiciendis rem quod accusamus ut quas doloremque veniam itaque tenetur laborum dicta vero optio exercitationem at nemo ratione consequatur, fugiat eos aperiam pariatur! Omnis, repellendus illum.</p>
+        </div>
+      );
+}
+ 
+export default About;
