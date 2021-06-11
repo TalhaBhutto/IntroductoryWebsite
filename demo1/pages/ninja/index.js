8dfeb7582bfdbbbb311c3cc84fa659d57361dc92
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-06-11 19:06:50 +0500

    ninja

--- demo1/pages/ninja/index.js
+++ demo1/pages/ninja/index.js
@@ -1,8 +1,29 @@
-const Ninja = () => {
-    return ( <div>
-        <h1>Ninja</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto tenetur temporibus ex? Doloremque hic voluptas asperiores. Eligendi, dignissimos numquam quia et modi eum molestias eius consequatur ratione vitae repudiandae alias ullam inventore. Suscipit facilis velit beatae optio in deleniti incidunt cupiditate nostrum aut maiores voluptates earum nobis delectus a adipisci laudantium et, veniam eos architecto molestiae quas ad, neque quisquam perferendis? Omnis a suscipit, quod illum inventore laborum. Aut vel asperiores natus unde praesentium, mollitia quibusdam explicabo non, dolorem odio dicta tempora quidem officia optio illo, accusamus ipsum aspernatur et ab corporis? Adipisci neque nisi incidunt quisquam quod exercitationem consequuntur?</p>
-    </div> );
+import styles from '../../styles/Jobs.module.css'
+
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+
+  return {
+    props: { ninjas: data }
+  }
+}
+
+const Ninjas = ({ ninjas }) => {
+  console.log(ninjas)
+
+  return (
+    <div>
+      <h1>All Ninjas</h1>
+      {ninjas.map(ninja => (
+        <div key={ninja.id}>
+          <a className={styles.single}>
+            <h3>{ ninja.name }</h3>
+          </a>
+        </div>
+      ))}
+    </div>
+  );
 }
  
-export default Ninja;
+export default Ninjas;
