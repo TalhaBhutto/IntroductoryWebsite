55a02c78f348a3f203d7ec2c1ac65cb7d72f4d9e
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-06-21 21:22:56 +0500

    a few changes

--- demo1/components/footer/footer.js
+++ demo1/components/footer/footer.js
@@ -1,10 +1,13 @@
 import Link from 'next/link'
 import styles from '../../styles/Home.module.css'
+import { useState,useEffect } from 'react';
 
 const Footer = () => {
+    const [color1, setColor1] = useState("black");
+    const [color2, setColor2] = useState("blue");
     return (
-        <> <Link className={styles.btn} href="https://memoriesbytalha.netlify.app/"><a>My Demo Project</a></Link>  |
-            <Link className={styles.btn} href="https://drive.google.com/file/d/14T4NUXkQAFN0KJ3Oau6DjbPnCQN7vEkx/view"><a>  My Complete CV</a></Link>
+        <> <Link href="https://memoriesbytalha.netlify.app/"><a className={styles.btn2}>My Demo Project</a></Link>  ||
+            <Link href="https://drive.google.com/file/d/14T4NUXkQAFN0KJ3Oau6DjbPnCQN7vEkx/view"><a  className={styles.btn3}>  My Complete CV</a></Link>
 
             <div>Copyright 2021 Talha Hussain</div>
         </>);
--- demo1/pages/about.js
+++ demo1/pages/about.js
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
 const About = () => {
     return (
         <><Head>
@@ -7,7 +9,7 @@ const About = () => {
         </Head>
             <div>
                 <h1>About</h1>
-                <p>Web Developers are responsible for creating websites that are both functionally and aesthetically pleasing to the user. The typical work activities of a Web Developer include back-end tasks such as developing databases, writing and testing code for website functions, troubleshooting code in existing features and brainstorming new features based on user feedback. I have trained myself on these principles. My goal is to do justice with my job or whichever task I am assigned. I put all my efforts to provide best result the client.</p>
+                <p className={styles.text}>Web Developers are responsible for creating websites that are both functionally and aesthetically pleasing to the user. The typical work activities of a Web Developer include back-end tasks such as developing databases, writing and testing code for website functions, troubleshooting code in existing features and brainstorming new features based on user feedback. I have trained myself on these principles. My goal is to do justice with my job or whichever task I am assigned. I put all my efforts to provide best result the client.</p>
                 </div>
         </>);
 }
